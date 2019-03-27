1a1fd5b28febd4eb9afb8eca198cef1d912290c8
Devarsh shukla <Devarsh14@gmail.com>
2019-03-27 14:58:31 +1000

    Added mongo db console application Create first add operation in mongo db.

---
+++ MongoClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var client = new MongoClient("");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MongoConsoleClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+namespace MongoConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string mongoDBConnectionString = "mongodb://localhost:27017";
+            var mongoClient = new MongoClient(mongoDBConnectionString);
+            var database = mongoClient.GetDatabase("startup");
+
+            var collection = database.GetCollection<BsonDocument>("employee");
+
+            var document = new BsonDocument
+            {
+                { "name","alex"},
+                { "age","28"},
+            };
+
+            collection.InsertOneAsync(document);
+
+            Console.Read();
+        }
+    }
+}
